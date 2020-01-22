3e41b75f0eb1fc3605ed92599e9c1ac4e440a503
Shawn Gibbons <sgibbons@police.local>
2020-01-21 21:39:33 -0500

    updates

--- routes/tasks.js
+++ routes/tasks.js
@@ -11,15 +11,17 @@ router.get('/doors', function (req, res, next) {
 	http.get("http://10.10.3.31:18779/Infinias/ia/doors/status?username=dispatch1&password=1234", resp => {
     let data = "";
 
-    // A chunk of data has been recieved.
-    resp.on("data", chunk => {
-      data += chunk;
-    });
+	    // A chunk of data has been recieved.
+	    resp.on("data", chunk => {
+	      data += chunk;
+	    });
 
-    // The whole response has been received. Print out the result.
-    resp.on("end", () => {
-      let doors = JSON.parse(data);
-      console.log(doors);
+	    // The whole response has been received. Print out the result.
+	    resp.on("end", () => {
+	      let doors = JSON.parse(data);
+	      console.log(doors);
+	  	});
+	});
 });
 
 module.exports = router;
