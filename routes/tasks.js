9bd9431365e58f14cf6a933addd79ee48008348d
Shawn Gibbons <sgibbons@police.local>
2020-01-21 21:50:21 -0500

    updates

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,19 +8,38 @@ router.get('/tasks', function (req, res, next) {
 });
 
 router.get('/doors', function (req, res, next) {
-	http.get("http://10.10.3.31:18779/Infinias/ia/doors/status?username=dispatch1&password=1234", resp => {
-    let data = "";
+	http.get('http://nodejs.org/dist/index.json', (res) => {
+	  const { statusCode } = res;
+	  const contentType = res.headers['content-type'];
 
-	    // A chunk of data has been recieved.
-	    resp.on("data", chunk => {
-	      data += chunk;
-	    });
+	  let error;
+	  if (statusCode !== 200) {
+	    error = new Error('Request Failed.\n' +
+	                      `Status Code: ${statusCode}`);
+	  } else if (!/^application\/json/.test(contentType)) {
+	    error = new Error('Invalid content-type.\n' +
+	                      `Expected application/json but received ${contentType}`);
+	  }
+	  if (error) {
+	    console.error(error.message);
+	    // Consume response data to free up memory
+	    res.resume();
+	    return;
+	  }
 
-	    // The whole response has been received. Print out the result.
-	    resp.on("end", () => {
-	      let doors = JSON.parse(data);
-	      console.log(doors);
-	  	});
+	  res.setEncoding('utf8');
+	  let rawData = '';
+	  res.on('data', (chunk) => { rawData += chunk; });
+	  res.on('end', () => {
+	    try {
+	      const parsedData = JSON.parse(rawData);
+	      console.log(parsedData);
+	    } catch (e) {
+	      console.error(e.message);
+	    }
+	  });
+	}).on('error', (e) => {
+	  console.error(`Got error: ${e.message}`);
 	});
 });
 
